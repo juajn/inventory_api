c2dd6a680bb2c9a11eecdc5bc7f1f07f4b3a2901
juajn <juandiegodh09@gmail.com>
2025-12-03 16:28:27 -0500

    api desplieguev4

--- app/db/session.py
+++ app/db/session.py
@@ -1,23 +1,17 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URI
+DATABASE_URI = settings.DATABASE_URI
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
+    DATABASE_URI,
     pool_pre_ping=True
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
-def get_db() -> Session:
+def get_db():
     db = SessionLocal()
     try:
         yield db
